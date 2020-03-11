a5f60681f0a60fdf6b1bd2886895e0ee34337b3f
maqroll <loteroc@gmail.com>
2020-03-11 08:15:11 +0100

    .

---
+++ data/src/test/java/org/apache/iceberg/data/BaseLearning.java
@@ -0,0 +1,153 @@
+package org.apache.iceberg.data;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableMap;
+import org.apache.commons.io.FileUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.iceberg.*;
+import org.apache.iceberg.avro.Avro;
+import org.apache.iceberg.data.avro.DataWriter;
+import org.apache.iceberg.data.parquet.GenericParquetWriter;
+import org.apache.iceberg.hadoop.HadoopInputFile;
+import org.apache.iceberg.hadoop.HadoopTables;
+import org.apache.iceberg.io.FileAppender;
+import org.apache.iceberg.parquet.Parquet;
+import org.apache.iceberg.transforms.Transform;
+import org.apache.iceberg.transforms.Transforms;
+import org.apache.iceberg.types.Types;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.apache.iceberg.hadoop.HadoopOutputFile.fromPath;
+import static org.apache.iceberg.types.Types.NestedField.optional;
+import static org.apache.iceberg.types.Types.NestedField.required;
+
+public class BaseLearning {
+  private static final Configuration CONF = new Configuration();
+  private static final Tables TABLES = new HadoopTables(CONF);
+  private static final File TABLE_LOCATION = new File("/tmp/iceberg");
+
+  private final FileFormat format;
+  private final Schema SCHEMA;
+
+  public BaseLearning(Schema schema, FileFormat format) {
+    this.SCHEMA = schema;
+    this.format = format;
+  }
+
+  private String sharedTableLocation = null;
+  private Table sharedTable = null;
+  private List<Record> file1Records = null;
+  private List<Record> file2Records = null;
+  private List<Record> file3Records = null;
+  private List<Record> file1FirstSnapshotRecords = null;
+  private List<Record> file2FirstSnapshotRecords = null;
+  private List<Record> file3FirstSnapshotRecords = null;
+  private List<Record> file1SecondSnapshotRecords = null;
+  private List<Record> file2SecondSnapshotRecords = null;
+  private List<Record> file3SecondSnapshotRecords = null;
+
+  private PartitionSpec partitionSpec;
+
+  public void dump(Table table) {
+    for (HistoryEntry historyEntry : sharedTable.history()) {
+      Snapshot snapshot = sharedTable.snapshot(historyEntry.snapshotId());
+      System.out.println(historyEntry.snapshotId() + "," + snapshot.manifestListLocation() + "," + snapshot.operation());
+      for (ManifestFile manifest : snapshot.manifests()) {
+        System.out.println(manifest.path());
+      }
+      ;
+    }
+  }
+
+  //@Before
+  public Table createTable(final String testPath) throws IOException {
+    File testLocation = new File(TABLE_LOCATION,testPath);
+    File location = new File(testLocation,format.toString());
+
+    FileUtils.deleteDirectory(location);
+    location.mkdirs();
+    this.sharedTableLocation = location.toString();
+
+    this.partitionSpec = PartitionSpec.builderFor(SCHEMA).bucket("id",10).build();
+
+    this.sharedTable = TABLES.create(
+        SCHEMA,
+        partitionSpec,
+        ImmutableMap.of(TableProperties.DEFAULT_FILE_FORMAT, format.name()),
+        sharedTableLocation);
+
+    return this.sharedTable;
+  }
+
+  public DataFile writeFile(String location, String filename, List<Record> records) throws IOException {
+    Path path = new Path(location, filename);
+    FileFormat fileFormat = FileFormat.fromFileName(filename);
+    Preconditions.checkNotNull(fileFormat, "Cannot determine format for file: %s", filename);
+
+    GenericRecord partition = null;
+
+    Transform<Long, Integer> bucket = Transforms.<Long>bucket(Types.LongType.get(), 10);
+    List<Integer> buckets = records.stream().map((e) -> bucket.apply((Long) e.getField("id"))).collect(Collectors.toList());
+    Integer min = buckets.stream().min(Comparator.comparingInt(a -> a)).get();
+    Integer max = buckets.stream().max(Comparator.comparingInt(a -> a)).get();
+    if (min.equals(max)) {
+      partition = GenericRecord.create(partitionSpec.partitionType());
+      partition.set(0,min);
+      System.out.println(filename + "-> partition:" + min);
+    } else {
+      System.out.println(filename + "-> partition N/A");
+    }
+
+    switch (fileFormat) {
+      case AVRO:
+        FileAppender avroAppender = Avro.write(fromPath(path, CONF))
+            .schema(SCHEMA)
+            .createWriterFunc(DataWriter::create)
+            .named(fileFormat.name())
+            .build();
+        try {
+          avroAppender.addAll(records);
+        } finally {
+          avroAppender.close();
+        }
+
+        DataFiles.Builder builder = DataFiles.builder(partitionSpec);
+        if (partition != null) { builder.withPartition(partition); }
+
+        return builder
+            .withInputFile(HadoopInputFile.fromPath(path, CONF))
+            .withMetrics(avroAppender.metrics())
+            .build();
+
+      case PARQUET:
+        FileAppender<Record> orcAppender = Parquet.write(fromPath(path, CONF))
+            .schema(SCHEMA)
+            .createWriterFunc(GenericParquetWriter::buildWriter)
+            .build();
+        try {
+          orcAppender.addAll(records);
+        } finally {
+          orcAppender.close();
+        }
+
+        DataFiles.Builder builder1 = DataFiles.builder(partitionSpec);
+
+        if (partition != null) { builder1.withPartition(partition);}
+
+        return builder1
+            .withInputFile(HadoopInputFile.fromPath(path, CONF))
+            .withMetrics(orcAppender.metrics())
+            .build();
+
+      default:
+        throw new UnsupportedOperationException("Cannot write format: " + fileFormat);
+    }
+  }
+}
---
+++ data/src/test/java/org/apache/iceberg/data/TestLearning.java
@@ -0,0 +1,144 @@
+package org.apache.iceberg.data;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import org.apache.commons.io.FileUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.iceberg.*;
+import org.apache.iceberg.avro.Avro;
+import org.apache.iceberg.data.avro.DataWriter;
+import org.apache.iceberg.data.parquet.GenericParquetWriter;
+import org.apache.iceberg.expressions.Expressions;
+import org.apache.iceberg.hadoop.HadoopInputFile;
+import org.apache.iceberg.hadoop.HadoopTables;
+import org.apache.iceberg.io.FileAppender;
+import org.apache.iceberg.parquet.Parquet;
+import org.apache.iceberg.types.Types;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.*;
+
+import static com.google.common.collect.Iterables.*;
+import static org.apache.iceberg.DataFiles.fromInputFile;
+import static org.apache.iceberg.expressions.Expressions.*;
+import static org.apache.iceberg.hadoop.HadoopOutputFile.fromPath;
+import static org.apache.iceberg.types.Types.NestedField.optional;
+import static org.apache.iceberg.types.Types.NestedField.required;
+
+@RunWith(Parameterized.class)
+public class TestLearning {
+  private static final Schema SCHEMA = new Schema(
+      required(1, "id", Types.LongType.get()),
+      optional(2, "data", Types.StringType.get()));
+
+  @Parameterized.Parameters
+  public static Object[][] parameters() {
+    return new Object[][] {
+        new Object[] { "parquet" },
+        new Object[] { "avro" }
+    };
+  }
+
+  private final FileFormat format;
+
+  public TestLearning(String format) {
+    this.format = FileFormat.valueOf(format.toUpperCase(Locale.ENGLISH));
+  }
+
+  private Table sharedTable = null;
+  private List<Record> file1Records = null;
+  private List<Record> file2Records = null;
+  private List<Record> file3Records = null;
+  private List<Record> file1FirstSnapshotRecords = null;
+  private List<Record> file2FirstSnapshotRecords = null;
+  private List<Record> file3FirstSnapshotRecords = null;
+  private List<Record> file1SecondSnapshotRecords = null;
+  private List<Record> file2SecondSnapshotRecords = null;
+  private List<Record> file3SecondSnapshotRecords = null;
+
+  private PartitionSpec partitionSpec;
+
+  @Test
+  public void testAppend() throws IOException {
+    BaseLearning base = new BaseLearning(SCHEMA,format);
+
+    sharedTable = base.createTable("TEST_APPEND");
+
+    Record record = GenericRecord.create(SCHEMA);
+
+    this.file1Records = new ArrayList<Record>();
+
+    file1Records.add(record.copy(ImmutableMap.of("id", 0L, "data", UUID.randomUUID().toString())));
+    DataFile file1 = base.writeFile(sharedTable.location(), format.addExtension("file-1"), file1Records);
+
+    this.file2Records = new ArrayList<Record>();
+    file2Records.add(record.copy(ImmutableMap.of("id", 1L, "data", UUID.randomUUID().toString())));
+    DataFile file2 = base.writeFile(sharedTable.location(), format.addExtension("file-2"), file2Records);
+
+    this.file3Records = new ArrayList<Record>();
+    file3Records.add(record.copy(ImmutableMap.of("id", 1L, "data", UUID.randomUUID().toString())));
+    DataFile file3 = base.writeFile(sharedTable.location(), format.addExtension("file-3"), file3Records);
+
+    sharedTable.newAppend()
+        .appendFile(file1)
+        .appendFile(file2)
+        .commit();
+
+    sharedTable.newAppend()
+        .appendFile(file3)
+        .commit();
+  }
+
+  @Test
+  public void testOverwrite() throws IOException {
+    BaseLearning base = new BaseLearning(SCHEMA,format);
+
+    sharedTable = base.createTable("TEST_OVERWRITE");
+
+    Record record = GenericRecord.create(SCHEMA);
+
+    this.file1Records = new ArrayList<Record>();
+    file1Records.add(record.copy(ImmutableMap.of("id", 60L, "data", UUID.randomUUID().toString())));
+    DataFile file1 = base.writeFile(sharedTable.location(), format.addExtension("file-1"), file1Records);
+
+    this.file2Records = new ArrayList<Record>();
+    file2Records.add(record.copy(ImmutableMap.of("id", 1L, "data", UUID.randomUUID().toString())));
+    DataFile file2 = base.writeFile(sharedTable.location(), format.addExtension("file-2"), file2Records);
+
+    this.file3Records = new ArrayList<Record>();
+    file3Records.add(record.copy(ImmutableMap.of("id", 1L, "data", UUID.randomUUID().toString())));
+    DataFile file3 = base.writeFile(sharedTable.location(), format.addExtension("file-3"), file3Records);
+
+    sharedTable.newAppend()
+        .appendFile(file1)
+        .commit();
+
+    sharedTable.newAppend()
+        .appendFile(file2)
+        .commit();
+
+    // Overwrite works on whole files
+    // Cannot delete file where some, but not all, rows match filter ref(name="id") == 1: file:/tmp/iceberg/TEST_OVERWRITE/AVRO/file-1.avro
+    sharedTable.newOverwrite()
+        //.overwriteByRowFilter(Expressions.alwaysTrue())
+        .overwriteByRowFilter(equal("id",1L))
+        .addFile(file3)
+        .commit();
+
+    base.dump(sharedTable);
+  }
+}
